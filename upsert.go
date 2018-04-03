8fda1d5f52dc4e157a1410206e98adf9c296eb17
tomogoma <ogomatom@gmail.com>
2018-04-03 16:05:52 +0300

    Add some primitive Update implementations

--- upsert.go
+++ upsert.go
@@ -13,6 +13,30 @@ type StringUpdate struct {
 func (su StringUpdate) IsUpdating() bool   { return su.Updating }
 func (su StringUpdate) Value() interface{} { return su.NewVal }
 
+type BoolUpdate struct {
+	Updating bool
+	NewVal   bool
+}
+
+func (su BoolUpdate) IsUpdating() bool   { return su.Updating }
+func (su BoolUpdate) Value() interface{} { return su.NewVal }
+
+type Int64Update struct {
+	Updating bool
+	NewVal   int64
+}
+
+func (su Int64Update) IsUpdating() bool   { return su.Updating }
+func (su Int64Update) Value() interface{} { return su.NewVal }
+
+type Float64Update struct {
+	Updating bool
+	NewVal   float64
+}
+
+func (su Float64Update) IsUpdating() bool   { return su.Updating }
+func (su Float64Update) Value() interface{} { return su.NewVal }
+
 // AppendUpdate adds the value of upd to updArgs and updCol (column to be updated) to updCols
 // if upd IsUpdating.
 // The result of appending to updCols and updArgs are returned respectively.
