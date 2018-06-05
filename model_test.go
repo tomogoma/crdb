a540cf7ac124bf3ac57661b63f03b9ce8c051004
tomogoma <ogomatom@gmail.com>
2018-06-05 13:37:45 +0300

    Test InstantiateDB

--- model_test.go
+++ model_test.go
@@ -8,6 +8,11 @@ import (
 
 	"github.com/tomogoma/crdb"
 	"gopkg.in/yaml.v2"
+	"github.com/pborman/uuid"
+	"strings"
+	"runtime"
+	"fmt"
+	"path/filepath"
 )
 
 var confFile = flag.String("conf", "dsn.conf.yml", "/path/to/testconf.yml")
@@ -100,10 +105,99 @@ func TestTryConnect(t *testing.T) {
 
 }
 
+func TestInstantiateDB(t *testing.T) {
+
+	t.Parallel()
+
+	tts := []struct {
+		name         string
+		dbNameSuffix string
+		tableDescs   []string
+		expErr       bool
+	}{
+		{
+			name:         "Ok",
+			dbNameSuffix: "",
+			tableDescs: []string{
+				"CREATE TABLE foos (name VARCHAR(25))",
+				"CREATE TABLE bars (name VARCHAR(25))",
+			},
+			expErr: false,
+		},
+		{
+			name:         "BadDBName",
+			dbNameSuffix: "-abc", // Hyphens(-) not allowed in db name in CockroachDB
+			tableDescs: []string{
+				"CREATE TABLE foos (name VARCHAR(25))",
+				"CREATE TABLE bars (name VARCHAR(25))",
+			},
+			expErr: true,
+		},
+		{
+			name:         "BadTableDesc",
+			dbNameSuffix: "",
+			tableDescs: []string{
+				"CREATE TABLE foos (name VARCHAR(25))",
+				"CREATE TABLE bars (name VARCHARS(25))", // VARCHARS is not valid SQL keyword
+			},
+			expErr: true,
+		},
+	}
+	conf := setUp(t)
+	var currConf crdb.Config
+
+	for _, tc := range tts {
+		t.Run(tc.name, func(t *testing.T) {
+
+			t.Parallel()
+
+			currConf = conf // No deep copy as long as crdb.Config.DBName remains a none-pointer.
+			currConf.DBName = randDBName(currConf.DBName) + tc.dbNameSuffix
+			db := newConn(t, currConf.FormatDSN())
+			defer tearDown(t, db, currConf.DBName)
+
+			err := crdb.InstantiateDB(db, currConf.DBName, tc.tableDescs...)
+			AssertNillable(t, !tc.expErr, err)
+		})
+	}
+}
+
+func AssertNillable(tb testing.TB, expNil bool, err error) {
+	if expNil == (err == nil) {
+		return
+	}
+	_, file, line, _ := runtime.Caller(1)
+	var msg string
+	if expNil {
+		msg = fmt.Sprintf("got error: %v", err.Error())
+	} else {
+		msg = "Expected an error, got nil"
+	}
+	fmt.Printf("\033[31m%s:%d: %s \033[39m\n\n", filepath.Base(file), line, msg)
+	tb.FailNow()
+}
+
+func randDBName(prefix string) string {
+	// Hyphens (-) are not acceptable db names in CockroachDB.
+	suffixs := strings.Split(uuid.New(), "-")
+	name := prefix
+	for _, suffix := range suffixs {
+		name = name + "_" + suffix
+	}
+	return name
+}
+
 func newConn(t *testing.T, DSN string) *sql.DB {
 	db, err := crdb.DBConn(DSN)
 	if err != nil {
 		t.Fatalf("Error setting up: crdb.DBConn(): %v", err)
 	}
 	return db
+}
+
+func tearDown(t *testing.T, db *sql.DB, dbName string) {
+	_, err := db.Exec("DROP DATABASE IF EXISTS " + dbName + " CASCADE")
+	if err != nil {
+		t.Fatalf("Failed teardown: %v", err)
+	}
 }
